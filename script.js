d7c56b05bf344079ed5c7ed5dd8bf6ef3fa3c13d
HAMzA <khanjadooon0987@gmail.com>
2026-06-21 14:44:10 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showDate() {
+    document.getElementById('butt').innerHTML = "Today: " + Date()
+}
+function handleSign() {
+    alert("You are Registered")
+    document.getElementById('welcome-msg').innerHTML = "Thanks For Registration";
+
+}
